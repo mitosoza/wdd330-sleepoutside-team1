d68b1767e2f8b621ebfe91cc30f0b996c45b7057
mitosoza <msozagonzalez@byupathway.edu>
2025-05-28 21:55:17 -0600

    - Added new href with product id - Created a template to display product details - New ProductDetails.mjs to add products to cart and render product details - New getParam method in utils.mjs

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,11 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+console.log("ProductId: ", productId);
+console.log("Product: ", dataSource.findProductById(productId));
