3f269a46f34537554cbb86ea390db27089304a4e
Frankroz <franjrm201518@hotmail.com>
2025-06-11 15:24:17 +0800

    Cart buttons quantities added - It updates on the localStorage and displays the total immediately

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,16 +7,6 @@ function renderCartContents() {
   const cartList = new ShoppingCart(cartItems, cartListeElement);
 
   cartList.init();
-
-  const total = cartItems.reduce((sum, item) => sum + Number(item.FinalPrice), 0);
-  const cartFooter = document.querySelector(".cart-footer");
-  const cartTotal = document.querySelector(".cart-total");
-  if (cartItems.length > 0) {
-    cartTotal.textContent = `Total: $${total.toFixed(2)}`;
-    cartFooter.hidden = false;
-  } else {
-    cartFooter.hidden = true;
-  }
 }
 
 loadHeaderFooter();
